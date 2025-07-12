047c8e6e605758aa9ec3c8d10caa40db23bbe881
abusalehaabu <salehaabu43@gmail.com>
2025-07-13 01:30:53 +0530

    update my js file

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ function calculate() {
     var num1 = document.getElementById("num").value;
     var num2 = document.getElementById("num2").value;
     var result = document.getElementById("result");
+    var result2 = document.getElementById("result2");
 
     var sum = parseFloat(num1) + parseFloat(num2);
     result.innerHTML = "Sum: " + sum;
+    var subtraction = parseFloat(num1) - parseFloat(num2);
+    result2.innerHTML = "Subtraction: " + subtraction;
 }
